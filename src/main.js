dc3812c2ef5f1c64594cd7b02efb4268f67854e3
Gilbert <gilbertbgarza@gmail.com>
2013-09-18 20:43:40 -0500

    Comment rendering example

--- src/main.js
+++ src/main.js
@@ -1,3 +1,36 @@
 
-// Remove this line when you start working
-alert('Hello World!');
+// Since the comment template never changes, we can keep this global
+var commentTemplateHtml = $('#templates .comment').html()
+var commentTemplate = _.template(commentTemplateHtml);
+
+var comments = [
+  { username: 'xXx_coolkid_xXx', message: 'dude ur almost as kool as me' },
+  { username: 'top_redditr', message: 'old' },
+  { username: 'l33tschool', message: 'w/e man u cnt b33t m3' }
+];
+
+var render = function () {
+  // Empty the comments content div so that we don't duplicate comments
+  $('#comments .content').empty();
+
+  // For each comment, generate html and add it to the page
+  for (var i = 0; i < comments.length; i += 1) {
+    var newCommentHtml = commentTemplate( comments[i] );
+    $('#comments .content').append(newCommentHtml);
+  }
+};
+
+$('.new-comment button').on('click', function (e) {
+  var newUsername = $('.new-comment [name=username]').val();
+  var newMessage = $('.new-comment [name=message]').val();
+  // Add new comment to our comments array
+  comments.push({
+    username: newUsername,
+    message: newMessage
+  });
+  // Re-render the comments so the new one shows up
+  render();
+});
+
+// Render initial comments on page load
+render();
